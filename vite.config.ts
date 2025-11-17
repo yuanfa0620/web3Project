a61c236d368d7ff47592879768ba45be6c9ba048
changmao gege <yuanfa0620@gmail.com>
2025-11-17 20:54:55 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -65,6 +65,7 @@ export default defineConfig(({ mode }): UserConfig => {
         '/api': {
           target: 'http://localhost:8080',
           changeOrigin: true,
+          secure: false,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
